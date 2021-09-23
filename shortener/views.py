b6384d0fd5236d1cccfc51519dc5d01501958d00
Frit027 <Frit_027@mail.ru>
2021-09-23 22:15:18 +0300

    token computation moved to class

--- shortener/views.py
+++ shortener/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from datetime import date, timedelta
-from hashids import Hashids
 from .forms import URLForm
+from .token_creator import TokenCreator
+from validators import url as valid_url
 
 
 # python manage.py makemigrations
@@ -10,16 +11,20 @@ from .forms import URLForm
 
 
 def shortener_index(request):
-    token = ''
-    if request.method == 'POST':
-        form = URLForm(request.POST)
-        if form.is_valid():
-            url = form.save(commit=False)
-            length = request.POST.get('url_length', 5)
-            long_url = form.cleaned_data['long_url']
-            url.token = Hashids(salt=long_url, min_length=length).encode(len(long_url))
-            token = url.token
-            url.expiry_at = date.today() + timedelta(days=7)
-            url.save()
+    form = URLForm(request.POST or None)
+    if request.method == 'GET':
+        token = 'GET'
+    elif form.is_valid():
+        long_url = form.cleaned_data['long_url']
+        if not valid_url(long_url):
+            return render(request, 'shortener/shortener_index.html', {'token': 'URL не корректен!', 'form': form})
 
-    return render(request, 'shortener/shortener_index.html', {'token': token, 'form': URLForm()})
+        url = form.save(commit=False)
+
+        requested_length = int(request.POST.get('requested_length', 5))
+        url.token = token = TokenCreator.get_token(long_url, requested_length)
+        url.expiry_at = date.today() + timedelta(days=7)
+        url.save()
+    else:
+        token = 'NOT_VALID'
+    return render(request, 'shortener/shortener_index.html', {'token': token, 'form': form})
