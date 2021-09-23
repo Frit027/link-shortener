38382580bdf118b91401a643a0bef72642586161
Frit027 <Frit_027@mail.ru>
2021-09-23 22:11:02 +0300

    deleted labels

--- shortener/forms.py
+++ shortener/forms.py
@@ -1,4 +1,3 @@
-from django.utils.translation import ugettext_lazy as _
 from django.forms import ModelForm, TextInput
 from .models import URL
 
@@ -6,13 +5,11 @@ from .models import URL
 class URLForm(ModelForm):
     class Meta:
         model = URL
-        fields = ['long_url']
+        fields = ('long_url',)
         widgets = {
             'long_url': TextInput(attrs={
+                'id': 'long_url',
                 'placeholder': 'https://en.wikipedia.org/wiki/The_Triumph_of_Cleopatra',
                 'autocomplete': 'off'
             })
-        }
-        labels = {
-            'long_url': _('long_url')
         }
